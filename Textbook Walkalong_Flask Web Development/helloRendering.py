8695cb2a7b9ab1342c2e311b0293ff9c2f222812
Soyinka Sowoolu <soyinkas1@gmail.com>
2024-03-18 18:53:07 +0000

    testing webform

--- Textbook Walkalong_Flask Web Development/helloRendering.py
+++ Textbook Walkalong_Flask Web Development/helloRendering.py
@@ -16,9 +16,15 @@ class NameForm(FlaskForm):
     name = StringField("What is your name", validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.now(UTC))
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        
+    return render_template('index.html', form=form, name=name, current_time=datetime.now(UTC))
 
 
 @app.route('/user/<name>')

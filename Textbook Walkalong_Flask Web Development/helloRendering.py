89ca0da2f42b35891fad53d3967954925d7ba0cf
Soyinka Sowoolu <soyinkas1@gmail.com>
2024-03-19 19:12:51 +0000

    databases started

--- Textbook Walkalong_Flask Web Development/helloRendering.py
+++ Textbook Walkalong_Flask Web Development/helloRendering.py
@@ -5,6 +5,7 @@ from datetime import datetime, UTC
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_migrate import migrate
 
 app = Flask(__name__)
 

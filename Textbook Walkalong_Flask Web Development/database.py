0c5e90eb8983981f1d9b7ef353a13f87f035dc6b
Soyinka Sowoolu <soyinkas1@gmail.com>
2024-05-21 18:20:36 +0100

    flask database update

--- Textbook Walkalong_Flask Web Development/database.py
+++ Textbook Walkalong_Flask Web Development/database.py
@@ -6,7 +6,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-'mysql://root:Olatunde1$@localhost/pdp_2021_db'
+'mysql://root:Olatunde1$@localhost/test_flask'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    def __repr__(self):
+        return '<Role %r>' % self.name

6fcbda5e500561f06ed02a8373ff8c591f9a1d21
Soyinka Sowoolu <soyinkas1@gmail.com>
2024-05-22 19:09:10 +0100

    sqlalchemy connected mysql localhost

--- Textbook Walkalong_Flask Web Development/database.py
+++ Textbook Walkalong_Flask Web Development/database.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-'mysql://root:Olatunde1$@localhost/test_flask'
+'mysql://root:Olatunde1$@localhost/pet_shop'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -15,5 +15,17 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
     def __repr__(self):
         return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    

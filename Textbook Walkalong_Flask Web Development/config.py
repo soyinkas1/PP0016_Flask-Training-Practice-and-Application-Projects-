80cf75e41feefe20c9b64cb81adcf621a524129f
Soyinka Sowoolu <soyinkas1@gmail.com>
2024-05-26 19:08:20 +0100

    large app structure started

---
+++ Textbook Walkalong_Flask Web Development/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'test_key'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    
